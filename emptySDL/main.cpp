0b2320307f598b7baded3d8c1e76a9b587ef4580
Bruno Cabrera <bruno.cabrera98@gmail.com>
2021-04-05 16:35:40 -0300

    Final Commit

--- emptySDL/main.cpp
+++ emptySDL/main.cpp
@@ -1,6 +1,41 @@
+#include <SDL.h>
+#include <SDL_opengl.h>
 #include <FreeImage.h>
 #include <iostream>
 
-void main() {
-	std::cout << "Funciona";
+// MAGIA
+extern "C" { FILE __iob_func[3] = { *stdin,*stdout,*stderr }; }
+#ifdef main
+#undef main
+#endif
+
+int main(int argc, char* argv[]) {
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		std::cerr << "There was an error\n";
+		exit(1);
+	}
+
+	// Flags for SDL (Double buffer, Hardware instead of Software, OPENGL)
+	Uint32 flags = SDL_DOUBLEBUF | SDL_HWSURFACE | SDL_OPENGL;
+
+	if (SDL_SetVideoMode(640, 480, 32, flags) == NULL) {
+		std::cerr << "There was an error\n :(";
+		exit(1);
+	}
+
+	glClearColor(0.3, 0.3, 0.3, 1);
+
+	SDL_Event event;
+
+	do {
+		glClear(GL_COLOR_BUFFER_BIT);
+		while (SDL_PollEvent(&event)) {
+			if (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE) {
+				return 0;
+			}
+		}
+		SDL_GL_SwapBuffers();
+	} while (true);
+
+	return 0;
 }
